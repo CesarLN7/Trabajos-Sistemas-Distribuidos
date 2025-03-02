db071c0adb6a26031880614370eb47404f2008bc
CesarLN7 <100451326@alumnos.uc3m.es>
2025-03-02 20:01:30 +0100

    xd

--- ejercicio_evaluable_1/claves/claves.c
+++ ejercicio_evaluable_1/claves/claves.c
@@ -1,5 +1,6 @@
 /* lógica almacenamiento de las funciones. return -1 cuando fallo*/
 
+/*
 int destroy() { return 1; };
 
 int set_value(int key, char *value1, int N_value2, double *V_value2, struct Coord value3) { return 1; };
@@ -11,3 +12,5 @@ int modify_value(int key, char *value1, int N_value2, double *V_value2, struct C
 int delete_key(int key) { return 1; };
 
 int exist(int key) { return 1; };
+
+*/
