fe4cf53ed46702752c82ca1ef00c5520cea03d4a
CesarLN7 <100451326@alumnos.uc3m.es>
2025-03-21 19:16:35 +0100

    makefile y ejecutables arreglados. testeado, compila. a falta de hacerlo concurrente y solucionar errores (posiblemente relacionados con readline)

--- ejercicio_evaluable_2/run-clients.sh
+++ ejercicio_evaluable_2/run-clients.sh
@@ -5,8 +5,8 @@ declare -r greenColour="\e[0;32m\033[1m"
 declare -r endColour="\033[0m\e[0m"
 declare -r redColour="\e[0;31m\033[1m"
 
-UNO="$(./runcliente1.sh &)"
-DOS="$(./runcliente2.sh &)"
+UNO="$(./run-cliente1.sh &)"
+DOS="$(./run-cliente2.sh &)"
 wait
 
 echo -e "${redColour}${UNO}${endColour}\n"
