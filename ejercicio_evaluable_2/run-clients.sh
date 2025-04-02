c3a5e61d4997ff20248468dddba4d474dd2b15a6
100451213 <you@example.com>
2025-04-02 13:18:00 +0200

    creados clientes

--- ejercicio_evaluable_2/run-clients.sh
+++ ejercicio_evaluable_2/run-clients.sh
@@ -2,12 +2,18 @@
 
 #Colours
 declare -r greenColour="\e[0;32m\033[1m"
-declare -r endColour="\033[0m\e[0m"
 declare -r redColour="\e[0;31m\033[1m"
+declare -r blueColour="\e[0;34m\033[1m"
+declare -r yellowColour="\e[0;33m\033[1m"
+declare -r endColour="\033[0m\e[0m"
 
 UNO="$(./run-cliente1.sh &)"
 DOS="$(./run-cliente2.sh &)"
+TRES="$(./run-cliente3.sh &)"
+CUATRO="$(./run-cliente4.sh &)"
 wait
 
 echo -e "${redColour}${UNO}${endColour}\n"
 echo -e "${greenColour}${DOS}${endColour}\n"
+echo -e "${blueColour}${TRES}${endColour}\n"
+echo -e "${yellowColour}${CUATRO}${endColour}\n"
