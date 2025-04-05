e89d51ae6995d8d338d9c28f866f892701091c87
CesarLN7 <100451326@alumnos.uc3m.es>
2025-04-05 12:32:25 +0200

    minor updates

--- ejercicio_evaluable_2/proxy-sock.c
+++ ejercicio_evaluable_2/proxy-sock.c
@@ -28,7 +28,7 @@ int is_N_value2_valid(int N_value2) {
 
 // Función para conectar con el servidor
 int connect_to_server() {
-    int ss;
+    int sc;
     struct sockaddr_in server_addr;
     char *ip = getenv("IP_TUPLAS");
     char *port_str = getenv("PORT_TUPLAS");
@@ -38,8 +38,8 @@ int connect_to_server() {
     }
     int port = atoi(port_str);
 
-    ss = socket(AF_INET, SOCK_STREAM, 0);
-    if (ss < 0) {
+    sc = socket(AF_INET, SOCK_STREAM, 0);
+    if (sc < 0) {
         perror("Error al crear socket");
         return -1;
     }
@@ -48,22 +48,22 @@ int connect_to_server() {
     server_addr.sin_port = htons(port);
     inet_pton(AF_INET, ip, &server_addr.sin_addr);
 
-    if (connect(ss, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+    if (connect(sc, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
         perror("Error al conectar con el servidor");
         return -1;
     }
-    return ss;
+    return sc;
 }
 
 int destroy() {
-    int ss = connect_to_server();
-    if (ss < 0) return -1;
+    int sc = connect_to_server();
+    if (sc < 0) return -1;
 
     char op[] = "1"; // Mandamos así el código de operación para que incluya el '\0' al final de la cadena
-    send_message(ss, op, strlen(op) + 1);
+    send_message(sc, op, strlen(op) + 1);
 
     char res[MAXSTR];
-    readLine(ss, res, MAXSTR);
+    readLine(sc, res, MAXSTR);
     return atoi(res);
 
 }
@@ -73,80 +73,80 @@ int set_value(int key, char *value1, int N_value2, double *V_value2, struct Coor
         return -1;
     }
 
-    int ss = connect_to_server();
-    if (ss < 0) {
+    int sc = connect_to_server();
+    if (sc < 0) {
         perror("Error al conectar con el servidor");
         return -1;
     }
 
     char op[] = "2";
-    send_message(ss, op, strlen(op) + 1);
+    send_message(sc, op, strlen(op) + 1);
 
     // Enviamos la clave al servidor
     char entero_a_cadena[MAXSTR];
     sprintf(entero_a_cadena, "%d", key);
-    send_message(ss, entero_a_cadena, strlen(entero_a_cadena) + 1);
+    send_message(sc, entero_a_cadena, strlen(entero_a_cadena) + 1);
 
     // Enviamos value1 al servidor
-    send_message(ss, value1, strlen(value1) + 1);
+    send_message(sc, value1, strlen(value1) + 1);
 
     // Enviamos N_value2 al servidor
     sprintf(entero_a_cadena, "%d", N_value2);
-    send_message(ss, entero_a_cadena, strlen(entero_a_cadena) + 1);
+    send_message(sc, entero_a_cadena, strlen(entero_a_cadena) + 1);
 
     // Enviamos V_value2 al servidor
     for (int i = 0; i < N_value2; i++) {
         sprintf(entero_a_cadena, "%lf", V_value2[i]);
-        send_message(ss, entero_a_cadena, strlen(entero_a_cadena) + 1);
+        send_message(sc, entero_a_cadena, strlen(entero_a_cadena) + 1);
     }
 
     // Enviamos value3 al servidor
     sprintf(entero_a_cadena, "%d", value3.x);
-    send_message(ss, entero_a_cadena, strlen(entero_a_cadena) + 1);
+    send_message(sc, entero_a_cadena, strlen(entero_a_cadena) + 1);
     sprintf(entero_a_cadena, "%d", value3.y);
-    send_message(ss, entero_a_cadena, strlen(entero_a_cadena) + 1);
+    send_message(sc, entero_a_cadena, strlen(entero_a_cadena) + 1);
 
     char res[MAXSTR];
-    readLine(ss, res, MAXSTR);
+    readLine(sc, res, MAXSTR);
     return atoi(res);
 }
 
 
 int get_value(int key, char *value1, int *N_value2, double *V_value2, struct Coord *value3) {
-    int ss = connect_to_server();
-    if (ss < 0) return -1;
+    int sc = connect_to_server();
+    if (sc < 0) return -1;
 
     char op[] = "3";
-    send_message(ss, op, strlen(op) + 1);
+    send_message(sc, op, strlen(op) + 1);
 
     // Enviamos la clave al servidor
     char entero_a_cadena[MAXSTR];
     sprintf(entero_a_cadena, "%d", key);
-    send_message(ss, entero_a_cadena, strlen(entero_a_cadena) + 1);
+    send_message(sc, entero_a_cadena, strlen(entero_a_cadena) + 1);
 
     // Leemos value1 del servidor
-    readLine(ss, entero_a_cadena, MAXSTR);
+    readLine(sc, entero_a_cadena, MAXSTR);
     strcpy(value1, entero_a_cadena);
 
     // Leemos N_value2 del servidor
-    readLine(ss, entero_a_cadena, MAXSTR);
+    readLine(sc, entero_a_cadena, MAXSTR);
     *N_value2 = atoi(entero_a_cadena);
 
     // Leemos V_value2 del servidor
     for (int i = 0; i < *N_value2; i++) {
-        readLine(ss, entero_a_cadena, MAXSTR);
+        readLine(sc, entero_a_cadena, MAXSTR);
         V_value2[i] = atof(entero_a_cadena);
     }
 
     // Leemos value3 del servidor
-    readLine(ss, entero_a_cadena, MAXSTR);
+    readLine(sc, entero_a_cadena, MAXSTR);
     value3->x = atoi(entero_a_cadena);
-    readLine(ss, entero_a_cadena, MAXSTR);
+    readLine(sc, entero_a_cadena, MAXSTR);
     value3->y = atoi(entero_a_cadena);
 
     // Leemos el resultado del servidor
     char res[MAXSTR];
-    readLine(ss, res, MAXSTR);
+    readLine(sc, res, MAXSTR);
     return atoi(res);
 
 }
@@ -156,73 +156,73 @@ int modify_value(int key, char *value1, int N_value2, double *V_value2, struct C
         return -1;
     }
 
-    int ss = connect_to_server();
-    if (ss < 0) return -1;
+    int sc = connect_to_server();
+    if (sc < 0) return -1;
 
     char op[] = "4";
-    send_message(ss, op, strlen(op) + 1);
+    send_message(sc, op, strlen(op) + 1);
 
     // Enviamos la clave al servidor
     char entero_a_cadena[MAXSTR];
     sprintf(entero_a_cadena, "%d", key);
-    send_message(ss, entero_a_cadena, strlen(entero_a_cadena) + 1);
+    send_message(sc, entero_a_cadena, strlen(entero_a_cadena) + 1);
 
     // Enviamos value1 al servidor
-    send_message(ss, value1, strlen(value1) + 1);
+    send_message(sc, value1, strlen(value1) + 1);
 
     // Enviamos N_value2 al servidor
     sprintf(entero_a_cadena, "%d", N_value2);
-    send_message(ss, entero_a_cadena, strlen(entero_a_cadena) + 1);
+    send_message(sc, entero_a_cadena, strlen(entero_a_cadena) + 1);
 
     // Enviamos V_value2 al servidor
     for (int i = 0; i < N_value2; i++) {
         sprintf(entero_a_cadena, "%lf", V_value2[i]);
-        send_message(ss, entero_a_cadena, strlen(entero_a_cadena) + 1);
+        send_message(sc, entero_a_cadena, strlen(entero_a_cadena) + 1);
     }
 
     // Enviamos value3 al servidor
     sprintf(entero_a_cadena, "%d", value3.x);
-    send_message(ss, entero_a_cadena, strlen(entero_a_cadena) + 1);
+    send_message(sc, entero_a_cadena, strlen(entero_a_cadena) + 1);
     sprintf(entero_a_cadena, "%d", value3.y);
-    send_message(ss, entero_a_cadena, strlen(entero_a_cadena) + 1);
+    send_message(sc, entero_a_cadena, strlen(entero_a_cadena) + 1);
 
     char res[MAXSTR];
-    readLine(ss, res, MAXSTR);
+    readLine(sc, res, MAXSTR);
     return atoi(res);
     
 }
 
 int delete_key(int key) {
-    int ss = connect_to_server();
-    if (ss < 0) return -1;
+    int sc = connect_to_server();
+    if (sc < 0) return -1;
 
     char op[] = "5";
-    send_message(ss, op, strlen(op) + 1);
+    send_message(sc, op, strlen(op) + 1);
     
     // Enviamos la clave al servidor
     char entero_a_cadena[MAXSTR];
     sprintf(entero_a_cadena, "%d", key);
-    send_message(ss, entero_a_cadena, strlen(entero_a_cadena) + 1);
+    send_message(sc, entero_a_cadena, strlen(entero_a_cadena) + 1);
 
     char res[MAXSTR];
-    readLine(ss, res, MAXSTR);
+    readLine(sc, res, MAXSTR);
     return atoi(res);
 }
 
 int exist(int key) {
-    int ss = connect_to_server();
-    if (ss < 0) return -1;
+    int sc = connect_to_server();
+    if (sc < 0) return -1;
 
     char op[] = "6"; 
-    send_message(ss, op, strlen(op) + 1);
+    send_message(sc, op, strlen(op) + 1);
 
     // Enviamos la clave al servidor
     char entero_a_cadena[MAXSTR];
     sprintf(entero_a_cadena, "%d", key);
-    send_message(ss, entero_a_cadena, strlen(entero_a_cadena) + 1);
+    send_message(sc, entero_a_cadena, strlen(entero_a_cadena) + 1);
     
     char res[MAXSTR];
-    readLine(ss, res, MAXSTR);
+    readLine(sc, res, MAXSTR);
 
     return atoi(res);
 }
